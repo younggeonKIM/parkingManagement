e90fb74779bb6016bbc41c04d12ccbd7e5f6c122
영건 김 <young3702@naver.com>
2026-08-09 13:46:23 +0900

    20260809_parkingMng_added

---
+++ parkingManagement/src/test/java/com/co/parking/ParkingManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.co.parking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParkingManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
